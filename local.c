6b7b9ee96b027e595d4032111e37ac14f07aaa14
Joseph Boudou <joseph.boudou@matabio.net>
2012-12-09 22:46:55 +0100

    Forgotten local.c file

---
+++ local.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "plan.h"
+#include "prob.h"
+
+extern Plan sch_random(Prob prob);
+
+void aux(Plan plan, void * data) {
+  if (*((Plan *)data) == NULL) {
+    *((Plan *)data) = plan;
+  }
+  else if (plan_duration(plan) < plan_duration(*((Plan *)data))) {
+    plan_free(*((Plan *)data));
+    *((Plan *)data) = (void*)plan;
+  }
+}
+
+Plan sch_localy(Prob prob) {
+  Plan plan = sch_random(prob);
+  Plan data;
+
+  ushort count = 0;
+  while (1) {
+    count += 1;
+    data = NULL;
+    plan_neighbourhood(plan, prob, &aux, &data);
+    if ((data == NULL) || (plan_duration(plan) <= plan_duration(data))) {
+      if (data != NULL) plan_free(data);
+      printf("%d iterations\n", count);
+      return plan;
+    }
+    plan_free(plan);
+    plan = data;
+  }
+
+}
